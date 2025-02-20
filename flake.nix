ec35a37547a34cc7acea093e6480ec3bb6cedd97
Antonio Ibarra <antonio.ibarra.g46@gmail.com>
2025-02-19 19:49:41 -0600

    Changes

--- flake.nix
+++ flake.nix
@@ -31,10 +31,10 @@
         spawnBrowser = if ((browser == "qutebrowser") && (wm == "hyprland")) then "qutebrowser-hyprprofile" else (if (browser == "qutebrowser") then "qutebrowser --qt-flag enable-gpu-rasterization --qt-flag enable-native-gpu-memory-buffers --qt-flag num-raster-threads=4" else browser); # Browser spawn command must be specail for qb, since it doesn't gpu accelerate by default (why?)
         defaultRoamDir = "Personal.p"; # Default org roam directory relative to ~/Org
         term = "alacritty"; # Default terminal command;
-        font = "Intel One Mono"; # Selected font
+        font = "IBM Plex"; # Selected font
 
-        # fontPkg = pkgs.ibm-flex; # Font package
-        fontPkg = pkgs.intel-one-mono;
+        fontPkg = pkgs.ibm-plex; # Font package
+        # fontPkg = pkgs.intel-one-mono;
         editor = "neovide"; # Default editor;
         # editor spawning translator
         # generates a command that can be used to spawn editor inside a gui
--- user/style/stylix.nix
+++ user/style/stylix.nix
@@ -47,33 +47,35 @@ in
   stylix.targets.alacritty.enable = false;
   programs.alacritty.settings = {
     colors = {
-      # TODO revisit these color mappings
-      # these are just the default provided from stylix
-      # but declared directly due to alacritty v3.0 breakage
-      primary.background = "#"+config.lib.stylix.colors.base00;
-      primary.foreground = "#"+config.lib.stylix.colors.base07;
-      cursor.text = "#"+config.lib.stylix.colors.base00;
-      cursor.cursor = "#"+config.lib.stylix.colors.base07;
-      normal.black = "#"+config.lib.stylix.colors.base00;
-      normal.red = "#"+config.lib.stylix.colors.base08;
-      normal.green = "#"+config.lib.stylix.colors.base0B;
-      normal.yellow = "#"+config.lib.stylix.colors.base0A;
-      normal.blue = "#"+config.lib.stylix.colors.base0D;
-      normal.magenta = "#"+config.lib.stylix.colors.base0E;
-      normal.cyan = "#"+config.lib.stylix.colors.base0B;
-      normal.white = "#"+config.lib.stylix.colors.base05;
-      bright.black = "#"+config.lib.stylix.colors.base03;
-      bright.red = "#"+config.lib.stylix.colors.base09;
-      bright.green = "#"+config.lib.stylix.colors.base01;
-      bright.yellow = "#"+config.lib.stylix.colors.base02;
-      bright.blue = "#"+config.lib.stylix.colors.base04;
-      bright.magenta = "#"+config.lib.stylix.colors.base06;
-      bright.cyan = "#"+config.lib.stylix.colors.base0F;
-      bright.white = "#"+config.lib.stylix.colors.base07;
+    primary = {
+      background = "#1E1E1E";  # Fondo principal oscuro
+      foreground = "#DCDCCC";  # Texto principal claro
     };
-    font.size = config.stylix.fonts.sizes.terminal;
-    font.normal.family = userSettings.font;
-  };
+    cursor = {
+      text = "#1E1E1E";    # Color del cursor (fondo del texto del cursor)
+      cursor = "#FFCC66";  # Color del cursor
+    };
+    normal = {
+      black = "#1E1E1E";   # Negro (fondo)
+      red = "#F2777A";     # Rojo (palabras clave)
+      green = "#99CC99";   # Verde (strings)
+      yellow = "#FFCC66";  # Amarillo (tipos)
+      blue = "#6699CC";    # Azul (funciones)
+      magenta = "#CC99CC"; # Magenta (built-ins)
+      cyan = "#66CCCC";    # Cyan (constantes)
+      white = "#DCDCCC";   # Blanco (texto general)
+    };
+    bright = {
+      black = "#262626";   # Negro brillante (fondo alternativo)
+      red = "#F99157";     # Rojo brillante
+      green = "#A6E22E";   # Verde brillante
+      yellow = "#FFD700";  # Amarillo brillante
+      blue = "#81A2BE";    # Azul brillante
+      magenta = "#B294BB"; # Magenta brillante
+      cyan = "#8ABEB7";    # Cyan brillante
+      white = "#F8F8F2";   # Blanco brillante
+    };
+  };  
   stylix.targets.kitty.enable = true;
   stylix.targets.gtk.enable = true;
   stylix.targets.rofi.enable = if (userSettings.wmType == "x11") then true else false;
